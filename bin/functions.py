c95c3f7e82e8bdb251a1db901a39036c2f12f596
idontfeelsogoood <msitko21@interia.pl>
2023-02-17 16:06:45 +0100

    Update the delete Function , added Edit Function, tested this new edit function and its working

--- bin/functions.py
+++ bin/functions.py
@@ -1,6 +1,6 @@
 import sqlite3
 from server import Server
-
+from time import sleep
 
 conn = sqlite3.connect('test.db')
 cursor = conn.cursor()
@@ -20,41 +20,72 @@ def get_server_names_from_database():
         server_names.append(record[0])
     return server_names
 
-def add_server_to_database(server:Server):
+def add_server_to_database(server:Server) -> bool:
     server_name = server.get_server_name()
     ip = server.get_server_ip()
     user = server.get_server_user()
     password = server.get_user_password()
     server_names = get_server_names_from_database()
     if server_name in server_names:
-        print("Server Already Exists in Database")
-        return 0
+        print("Server Already Exists in Database FAILED")
+        return False
 
     cursor.execute("INSERT INTO servers(server_name,ip,user,password) VALUES(:server_name,:ip,:user,:password)",
                    {'server_name':server_name, 'ip':ip, 'user':user, 'password':password})
     conn.commit()
+    print(f"Server {server.get_server_name()} added Successfully to Database")
+    return True
 
-def delete_server_from_database(server:Server):
+def delete_server_from_database(server:Server) -> bool:
     server_name = server.get_server_name()
     ip = server.get_server_ip()
     user = server.get_server_user()
     password = server.get_user_password()
     server_names = get_server_names_from_database()
     if server_name not in server_names:
-        print("Server Doesn't Exists in Database")
-        return 0
+        print("Server Doesn't Exists in Database FAILED")
+        return False
 
     cursor.execute("DELETE FROM servers WHERE server_name=:server_name ",{'server_name':server_name})
-
-def edit_server_in_database(server:Server):
-
-
-
-
-
-
-
     conn.commit()
+    print(f"Server {server.get_server_name()} Deleted Successfully from Database")
+    return True
+
+
+def edit_server_in_database(server:Server,new_server_name="",new_ip="",new_user="",new_password=""):
+
+    server_name = server.get_server_name()
+    server_name_before_change = server.get_server_name()
+    ip = server.get_server_ip()
+    user = server.get_server_user()
+    password = server.get_user_password()
+    server_names = get_server_names_from_database()
+
+    if server_name in server_names:
+        #if user doesnt give all informations
+        if (new_server_name == "") or (new_ip == "") or (new_user == "") or (new_password == ""):
+            if new_server_name == "":
+                new_server_name = server_name
+            if new_ip == "":
+                new_ip  = ip
+            if new_user == "":
+                new_user = user
+            if new_password == "":
+                new_password = password
+
+        server_name = new_server_name
+        ip = new_ip
+        user = new_user
+        password = new_password
+
+        cursor.execute("UPDATE servers SET server_name=:new_server_name,ip=:new_ip,user=:new_user,password=:new_password WHERE server_name=:server_name"
+                       ,{'new_server_name':server_name,'new_ip':ip,'new_user':user,'new_password':password,'server_name':server_name_before_change})
+        conn.commit()
+
+    else:
+        print(f"Server {server.get_server_name()} is not in Database FAILED")
+        return False
+
 
 
 
--- bin/main.py
+++ bin/main.py
@@ -2,7 +2,8 @@ from functions import *
 from server import Server
 
 def main():
-    server1 = Server("gni-training-322","10.1.1.118","sadmin","softel13")
+    server1 = Server("gni-training-04","10.1.1.118","sadmin","softel13")
     add_server_to_database(server1)
     #delete_server_from_database(server1)
+    #edit_server_in_database(server1,"Changed_training-04_1","10.1.1.90909","changed","pass")
 main()
