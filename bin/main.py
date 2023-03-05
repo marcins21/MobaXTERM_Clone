49b7929d6196c0bb10f1d01bab27094fa2b04132
marcins21 <62626012+marcins21@users.noreply.github.com>
2023-03-05 14:48:03 +0100

    refactoring

--- bin/main.py
+++ bin/main.py
@@ -5,15 +5,13 @@ from server import Server
 
 def main():
 
-
-
-    server1 = Server(f"gni-training-01","10.1.1.118","sadmin","softel13")
+    server1 = Server(f"Dns-name","192.168.0.1","sadmin","pass")
     #show_server_info(server1,validate_sever(server1))
     # add_server_to_database(server1)
     # delete_server_from_database(server1)
     # edit_server_in_database(server1,"Changed_training-04_1","10.1.1.90909","changed","pass")
 
-    user1 = User("marcin","Pass","Simple user")
+    user1 = User("username","Pass","Simple user")
     #valid_user(user1)
     #add_user_to_database(user1)
     #delete_user_from_database(user1)
